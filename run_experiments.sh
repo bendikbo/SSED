cb72c0da1fc77a053cd3c86d8d318601744d8586
Bendik Bogfjellmo <35365411+bendikbo@users.noreply.github.com>
2021-06-30 19:55:26 +0200

    Update run_experiments.sh

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/bash
 
+#Use this script to run through all configuration files
+#in a chosen directory. Script will run all training sessions
+#and dump the seission outputs as a log in outputs/
+
 cd "$(dirname "$0")"
 proj_dir=$(dirname "$0")
 conf_dir="configs"
+#Cange experiment dir to run with config files in your chosen directory
 experiment_dir="rg3"
 for config_file in "${proj_dir}/${conf_dir}/${experiment_dir}"/*
 do
